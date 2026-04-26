d291922d73ace3e02d3a8cf15e610e47b5b721b5
Jesús David Cantillo Romero <jesusdavidcantilloromero.10@gmail.com>
2026-04-25 21:32:07 -0500

    Añadir módulos Prisma y Categories al módulo principal de la aplicación.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
